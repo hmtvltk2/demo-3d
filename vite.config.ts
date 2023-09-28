72670e7a2297a23483813255c986305c0ba4b88b
Huynh Minh Tri <huynhminhtri2412@gmail.com>
2023-09-28 11:18:55 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: './docs'
-  },
+  base: '/demo-3d',
+
   plugins: [vue(), visualizer({ open: true, })],
 })
